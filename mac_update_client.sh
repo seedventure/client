0b3b8e4ffc5f7ff90a58f64e97565fac2c1be507
Marco Vasapollo <ceo@metaring.com>
2019-09-24 18:39:28 +0200

    New Mac update client file

--- mac_update_client.sh
+++ mac_update_client.sh
@@ -3,7 +3,10 @@ cd /home/vasa/client
 git fetch && git reset --hard origin/master
 npm run pack:osx
 rm ./dist/SEEDVenture-darwin-x64.zip
-zip ./dist/SEEDVenture-darwin-x64.zip ./dist/SEEDVenture-darwin-x64
+cd dist/SEEDVenture-darwin-x64
+zip -9 -r ../SEEDVenture-darwin-x64.zip ./
+cd ../../
 git add ./dist/SEEDVenture-darwin-x64.zip
+git lfs track ./dist/SEEDVenture-darwin-x64.zip
 git commit -a -m "[Automatic Bot] MAC Version"
 git push origin master
