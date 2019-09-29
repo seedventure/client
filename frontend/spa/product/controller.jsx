2116a30e6cf2692e6cd4fff3b167d0161b477f4b
Marco Vasapollo <ceo@metaring.com>
2019-09-29 10:30:56 +0200

    Basket Unsuitable Bugfix

--- frontend/spa/product/controller.jsx
+++ frontend/spa/product/controller.jsx
@@ -3,9 +3,6 @@ var ProductController = function (view) {
     context.view = view;
 
     context.makeUnsuitable = async function makeUnsuitable(product) {
-        var contract = new web3.eth.Contract(contracts.FundingPanel);
-        var method = contract.methods.setNewSeedMaxSupply(0);
-        method = method.encodeABI();
-        await client.blockchainManager.sendSignedTransaction(await client.userManager.signTransaction(product.address, method), "Make Basket Unsuitable");
+        await client.contractsManager.FundingPanel.setNewSeedMaxSupply(product.fundingPanelAddress, 0, "Make Basket Unsuitable");
     };
 };
