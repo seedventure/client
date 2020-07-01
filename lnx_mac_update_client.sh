e1948897362bdd5a58eb766aea1390af6b39a724
Marco Vasapollo <ceo@metaring.com>
2020-07-01 18:43:02 +0200

    Linux

--- lnx_mac_update_client.sh
+++ lnx_mac_update_client.sh
@@ -8,5 +8,12 @@ zip -9 -r ../SEEDVenture-darwin-x64.zip ./
 cd ../../
 git add ./dist/SEEDVenture-darwin-x64.zip
 git lfs track ./dist/SEEDVenture-darwin-x64.zip
-git commit -a -m "[Automatic Bot] MAC Version"
+npm run pack:lnx64
+rm ./dist/SEEDVenture-linux-x64.zip
+cd dist/SEEDVenture-linux-x64
+zip -9 -r ../SEEDVenture-linux-x64.zip ./
+cd ../../
+git add ./dist/SEEDVenture-linux-x64.zip
+git lfs track ./dist/SEEDVenture-linux-x64.zip
+git commit -a -m "[Automatic Bot] MAC and Linux Version"
 git push origin master
