4f22f5bd40ce1d5d2cdd09c23ed287cc0a9e085f
Deabit <info@deabit.net>
2020-09-04 15:22:42 +0200

    commit changes main

--- backend/main.js
+++ backend/main.js
@@ -99,7 +99,6 @@ app.on('ready', async function () {
     electronWindow.maximize();
     globalShortcut.register('f5', loadData);
     globalShortcut.register('CommandOrControl+R', loadData);
-    globalShortcut.register('Ctrl+I', ()=> electronWindow.webContents.openDevTools())
     loadData();
     (debug || test || dist) && electronWindow.webContents.openDevTools();
 });
